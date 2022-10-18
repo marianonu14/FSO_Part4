356789543bb8ec879923943c2290bb7896787452
Mariano <marianonunezma@gmail.com>
2022-10-18 15:15:34 -0300

    Refactoring, Folder Structure

---
+++ controllers/blogRouter.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const Blog = require('../models/blog');
+
+router.get('/', (req, res) => {
+  Blog.find({}).then((blogs) => {
+    res.send(blogs);
+  });
+});
+
+router.post('/', (req, res) => {
+  const blog = new Blog(req.body);
+
+  blog.save().then((result) => {
+    response.status(201).send(result);
+  });
+});
+
+module.exports = router;
---
+++ models/blog.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const config = require('../utils/config');
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    console.log('DB Connected');
+  })
+  .catch((err) => console.log(err.message));
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+module.exports = Blog
